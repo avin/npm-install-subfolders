d3958bc713a3b540eb64c14f80815325571cf726
a.vinogradov <a.vinogradov@bpcbt.com>
2022-09-25 01:16:12 +0300

    sync

--- main.js
+++ main.js
@@ -1,64 +1,73 @@
 const fs = require('fs');
 const os = require('os');
-const childProcess = require('child_process');
+const { spawnSync } = require('child_process');
 const path = require('path');
+const which = require('which');
 
-const walk = function (dir, done) {
+const walk = function(dir) {
   let results = [];
-  fs.readdir(dir, function (err, list) {
-    if (err) return done(err);
-    let i = 0;
-    (function next() {
-      let file = list[i++];
-      if (!file) {
-        return done(null, results);
-      }
-      file = path.resolve(dir, file);
-      fs.stat(file, function (err, stat) {
-        if (stat && stat.isDirectory()) {
-          walk(file, function (err, res) {
-            results = results.concat(res);
-            next();
-          });
-        } else {
-          results.push(file);
-          next();
-        }
-      });
-    })();
-  });
+  const list = fs.readdirSync(dir);
+
+  let i = 0;
+  (function next() {
+    let file = list[i++];
+    if (!file) {
+      return;
+    }
+    file = path.resolve(dir, file);
+    const stat = fs.statSync(file);
+    if (stat && stat.isDirectory()) {
+      const res = walk(file);
+      results = results.concat(res);
+      next();
+    } else {
+      results.push(file);
+      next();
+    }
+  })();
+
+  return results;
+
 };
+
+function getVoltaPrefix() {
+  const VOLTA_PREFIX = 'volta run';
+  const hasVoltaCommand = which.sync('volta', { nothrow: true }) !== null;
+  return hasVoltaCommand ? VOLTA_PREFIX : '';
+}
 
 function npmInstallSubfolders({ rootFolder, verbose, client, postAction }) {
   client = client || 'npm';
 
-  walk(rootFolder, function (err, results) {
-    if (err) throw err;
+  const results = walk(rootFolder);
 
-    results.forEach((fileSrc) => {
-      if (fileSrc.endsWith(path.sep + 'package.json') && !/node_modules/.exec(fileSrc)) {
-        const filePath = fileSrc.replace(path.sep + 'package.json', '');
+  results.forEach((fileSrc) => {
+    if (fileSrc.endsWith(path.sep + 'package.json') && !/node_modules/.exec(fileSrc)) {
+      const filePath = fileSrc.replace(path.sep + 'package.json', '');
 
-        if (verbose) {
-          console.log('===================================================================');
-          console.log(`Performing "${client} install" inside ${filePath}`);
-          console.log('===================================================================');
-        }
-
-        const npmCmd = os.platform().startsWith('win') ? `${client}.cmd` : client;
-
-        childProcess.spawn(npmCmd, ['install'], {
-          env: process.env,
-          cwd: filePath,
-          stdio: 'inherit',
-        });
-
-        if (postAction) {
-          postAction(filePath);
-        }
+      if (verbose) {
+        console.log('===================================================================');
+        console.log(`Performing "${client} install" inside ${filePath}`);
+        console.log('===================================================================');
       }
-    });
+
+      const npmCommand = os.platform().startsWith('win') ? `${client}.cmd` : client;
+
+      const command = `${getVoltaPrefix()} ${npmCommand} install`;
+
+      spawnSync(command, {
+        stdio: 'inherit',
+        shell: true,
+        env: process.env,
+        cwd: filePath,
+      });
+
+      if (postAction) {
+        postAction(filePath);
+      }
+    }
   });
+
 }
 
 module.exports = npmInstallSubfolders;
